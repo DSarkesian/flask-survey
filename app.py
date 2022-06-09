64960d35d32b09055dbef506aeb16d2f36184ec2
Jordan Asano <jordanasano@yahoo.com>
2022-06-09 12:13:48 -0700

    finished part 3

--- app.py
+++ app.py
@@ -17,4 +17,22 @@ def survey_start():
     instructions = survey.instructions
     title = survey.title
 
-    return render_template("survey_start.html",title=title,instructions=instructions)
+    return render_template("survey_start.html",
+        title=title,instructions=instructions)
+
+@app.post('/begin')
+def send_to_question():
+    """ Redirects to the url to show the first question """
+
+    return redirect(f"/questions/0")
+
+@app.get('/questions/<question_nmbr>')
+def show_questions(question_nmbr):
+    """ Displays the question based on the index input into the url"""
+    question = survey.questions[int(question_nmbr)].question
+    choices = survey.questions[int(question_nmbr)].choices
+    
+    return render_template('question.html', question_text=question, choices=choices)
+    # if int(question) < len(survey.questions) - 1:
+    #     return redirect(f'/question/{int(question) + 1}', 
+    #     question.question= survey.questions[int(question) + 1].question)
